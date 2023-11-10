0c8695f97ca241d194abc6c12d7680332b368f15
yamamon_hayaton <a201136@a.tsukuba-tech.ac.jp>
2023-11-10 12:29:47 +0900

    [add] about.html, help.html [add] terminal-history:local storage

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-document.getElementById('command-line').addEventListener('keypress', function (e)
-{
-	if (e.key === 'Enter')
-	{
-		e.preventDefault();
-		var command = this.value.trim();
-		switch (command)
-		{
-			case 'cd portfolio':
-				window.location.href = '/portfolio.html';
-				break;
-			default:
-				break;
+document.addEventListener('DOMContentLoaded', (event) => {
+	const commandLine = document.getElementById('command-line');
+	const terminalHistory = document.getElementById('terminal-history');
+	const history = JSON.parse(localStorage.getItem('terminalHistory')) || [];
+	history.forEach(cmd => {
+		const commandOutput = document.createElement('div');
+		commandOutput.textContent = `> ${cmd}`;
+		terminalHistory.appendChild(commandOutput);
+	});
+	commandLine.addEventListener('keydown', function (e) {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			const command = this.value.toLowerCase();
+			const commandOutput = document.createElement('div');
+			commandOutput.textContent = `> ${command}`;
+			terminalHistory.appendChild(commandOutput);
+			history.push(command);
+			localStorage.setItem('terminalHistory', JSON.stringify(history));
+			if (command === 'cd home') {
+				window.location.href = 'index.html';
+			} else if (command === 'cd about') {
+				window.location.href = 'about.html';
+			} else if (command === 'cd help') {
+				window.location.href = 'help.html';
+			} else {
+				commandOutput.textContent = `Command not found: ${command}`;
+			}
+			this.value = '';
 		}
-		this.value = ''; // コマンドラインをクリア
-	}
+	});
+});
+
+document.addEventListener('DOMContentLoaded', (event) =>
+{
+	const navLinks = document.querySelectorAll('.global-nav a');
+	const currentUrl = window.location.pathname.split('/').pop();
+
+	navLinks.forEach(link =>
+	{
+		if (link.getAttribute('href') === currentUrl)
+		{
+			link.parentElement.classList.add('current');
+		}
+		else
+		{
+			link.parentElement.classList.remove('current');
+		}
+	});
 });
